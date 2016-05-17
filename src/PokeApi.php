039d7073bcd02d262abe3598614de983e6e8dc67
Dan Rovito <DanRovito@users.noreply.github.com>
2016-05-17 09:40:45 -0400

    Create PokeApi.php

---
+++ src/PokeApi.php
@@ -0,0 +1,28 @@
+<?php
+
+class PokeApi
+{
+
+    public function sendRequest($uri)
+    {
+        //return $uri;
+        $ch = curl_init();
+
+        $timeout = 5;
+
+        curl_setopt($ch, CURLOPT_URL, $uri);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $data = curl_exec($ch);
+
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+
+        if ($http_code != 200) {
+            return json_encode('An error has occured. Check that your API key is correct');
+        }
+
+        return $data;
+    }
+}
