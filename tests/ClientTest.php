77f45a118e16c2bf373b8ea00306d3dc6eac31c7
Dan Rovito <DanRovito@users.noreply.github.com>
2016-06-03 08:49:39 -0400

    Create ClientTest.php

---
+++ tests/ClientTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testResourceList()
+    {
+        $poke = new PokeApi();
+
+        $response = $poke->resourceList('evolution-chain', '20', '20');
+
+        $this->assertJson($response, 'message');
+    }
+}
