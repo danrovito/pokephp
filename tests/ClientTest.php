cd55b150b1f58f936a851511cd085e2597d3422c
Dan Rovito <danrovito@gmail.com>
2016-07-16 23:28:10 -0400

    tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -19,29 +19,6 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 
         $response = $poke->berry('1');
 
-        //json files to match
-        $berryJson = json_decode('/responses/berry.json');
-        $responseJson = json_decode($response);
-
-        $this->assertTrue(json_match($responseJson, $berryJson));
-
-        //$this->assertJson($response, 'message');
-    }
-
-    //Check for matching json
-    public function json_match($a, $b)
-    {
-        if (count(array_diff_assoc($a, $b))) {
-            return false;
-        }
-        // we know that the indexes, but maybe not values, match.
-        // compare the values between the two arrays
-        foreach($a as $k => $v) {
-            if ($v !== $b[$k]) {
-                return false;
-            }
-        }
-        // we have identical indexes, and no unequal values
-        return true;
+        $this->assertJson($response, 'message');
     }
 }
