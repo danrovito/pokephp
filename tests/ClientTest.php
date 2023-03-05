8feed6a1b1b1f30b0c8404e30c5a3a89fc2c532c
AssoDePicche <samuelprado730@gmail.com>
2023-03-05 02:16:34 -0300

    test: update client test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,23 +1,24 @@
 <?php
 
+use PokePHP\Filter;
 use PokePHP\PokeApi;
 
-class ClientTest extends \PHPUnit_Framework_TestCase
+final class ClientTest extends \PHPUnit\Framework\TestCase
 {
     public function testResourceList()
     {
-        $poke = new PokeApi();
+        $poke = new PokeApi;
 
-        $response = $poke->resourceList('evolution-chain', '20', '20');
+        $response = $poke->search(Filter::EVOLUTION_CHAIN)->limit(20)->request();
 
         $this->assertJson($response, 'message');
     }
 
     public function testBerry()
     {
-        $poke = new PokeApi();
+        $poke = new PokeApi;
 
-        $response = $poke->berry('1');
+        $response = $poke->search(Filter::BERRY, 1)->request();
 
         $this->assertJson($response, 'message');
     }
