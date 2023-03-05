a11ef12023522c3adab678f37e639f27d2548a7f
AssoDePicche <samuelprado730@gmail.com>
2023-03-04 22:39:23 -0300

    refactor: extract base url from contructor to a constant

--- src/PokeApi.php
+++ src/PokeApi.php
@@ -4,343 +4,340 @@ namespace PokePHP;
 
 class PokeApi
 {
-    public function __construct()
-    {
-        $this->baseUrl = 'https://pokeapi.co/api/v2/';
-    }
+    private const URL = 'https://pokeapi.co/api/v2/';
 
     public function resourceList($endpoint, $limit = null, $offset = null)
     {
-        $url = $this->baseUrl.$endpoint.'/?limit='.$limit.'&offset='.$offset;
+        $url = self::URL . $endpoint . '/?limit=' . $limit . '&offset=' . $offset;
 
         return $this->sendRequest($url);
     }
 
     public function berry($lookUp)
     {
-        $url = $this->baseUrl.'berry/'.$lookUp;
+        $url = self::URL . 'berry/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function berryFirmness($lookUp)
     {
-        $url = $this->baseUrl.'berry-firmness/'.$lookUp;
+        $url = self::URL . 'berry-firmness/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function berryFlavor($lookUp)
     {
-        $url = $this->baseUrl.'berry-flavor/'.$lookUp;
+        $url = self::URL . 'berry-flavor/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function contestType($lookUp)
     {
-        $url = $this->baseUrl.'contest-type/'.$lookUp;
+        $url = self::URL . 'contest-type/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function contestEffect($id)
     {
-        $url = $this->baseUrl.'contest-effect/'.$id;
+        $url = self::URL . 'contest-effect/' . $id;
 
         return $this->sendRequest($url);
     }
 
     public function superContestEffect($id)
     {
-        $url = $this->baseUrl.'super-contest-effect/'.$id;
+        $url = self::URL . 'super-contest-effect/' . $id;
 
         return $this->sendRequest($url);
     }
 
     public function encounterMethod($lookUp)
     {
-        $url = $this->baseUrl.'encounter-method/'.$lookUp;
+        $url = self::URL . 'encounter-method/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function encounterCondition($lookUp)
     {
-        $url = $this->baseUrl.'encounter-condition/'.$lookUp;
+        $url = self::URL . 'encounter-condition/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function encounterConditionValue($lookUp)
     {
-        $url = $this->baseUrl.'encounter-condition-value/'.$lookUp;
+        $url = self::URL . 'encounter-condition-value/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function evolutionChain($id)
     {
-        $url = $this->baseUrl.'evolution-chain/'.$id;
+        $url = self::URL . 'evolution-chain/' . $id;
 
         return $this->sendRequest($url);
     }
 
     public function evolutionTrigger($lookUp)
     {
-        $url = $this->baseUrl.'evolution-trigger/'.$lookUp;
+        $url = self::URL . 'evolution-trigger/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function gameGeneration($lookUp)
     {
-        $url = $this->baseUrl.'generation/'.$lookUp;
+        $url = self::URL . 'generation/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokedex($lookUp)
     {
-        $url = $this->baseUrl.'pokedex/'.$lookUp;
+        $url = self::URL . 'pokedex/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function version($lookUp)
     {
-        $url = $this->baseUrl.'version/'.$lookUp;
+        $url = self::URL . 'version/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function versionGroup($lookUp)
     {
-        $url = $this->baseUrl.'version-group/'.$lookUp;
+        $url = self::URL . 'version-group/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function item($lookUp)
     {
-        $url = $this->baseUrl.'item/'.$lookUp;
+        $url = self::URL . 'item/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function itemAttribute($lookUp)
     {
-        $url = $this->baseUrl.'item-attribute/'.$lookUp;
+        $url = self::URL . 'item-attribute/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function itemCategory($lookUp)
     {
-        $url = $this->baseUrl.'item-category/'.$lookUp;
+        $url = self::URL . 'item-category/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function itemFlingEffect($lookUp)
     {
-        $url = $this->baseUrl.'item-fling-effect/'.$lookUp;
+        $url = self::URL . 'item-fling-effect/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function itemPocket($lookUp)
     {
-        $url = $this->baseUrl.'item-pocket/'.$lookUp;
+        $url = self::URL . 'item-pocket/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function move($lookUp)
     {
-        $url = $this->baseUrl.'move/'.$lookUp;
+        $url = self::URL . 'move/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function moveAilment($lookUp)
     {
-        $url = $this->baseUrl.'move-ailment/'.$lookUp;
+        $url = self::URL . 'move-ailment/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function moveBattleStyle($lookUp)
     {
-        $url = $this->baseUrl.'move-battle-style/'.$lookUp;
+        $url = self::URL . 'move-battle-style/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function moveCategory($lookUp)
     {
-        $url = $this->baseUrl.'move-category/'.$lookUp;
+        $url = self::URL . 'move-category/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function moveDamageClass($lookUp)
     {
-        $url = $this->baseUrl.'move-damage-class/'.$lookUp;
+        $url = self::URL . 'move-damage-class/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function moveLearnMethod($lookUp)
     {
-        $url = $this->baseUrl.'move-learn-method/'.$lookUp;
+        $url = self::URL . 'move-learn-method/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function moveTarget($lookUp)
     {
-        $url = $this->baseUrl.'move-target/'.$lookUp;
+        $url = self::URL . 'move-target/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function location($id)
     {
-        $url = $this->baseUrl.'location/'.$id;
+        $url = self::URL . 'location/' . $id;
 
         return $this->sendRequest($url);
     }
 
     public function locationArea($id)
     {
-        $url = $this->baseUrl.'location-area/'.$id;
+        $url = self::URL . 'location-area/' . $id;
 
         return $this->sendRequest($url);
     }
 
     public function palParkArea($lookUp)
     {
-        $url = $this->baseUrl.'pal-park-area/'.$lookUp;
+        $url = self::URL . 'pal-park-area/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function region($lookUp)
     {
-        $url = $this->baseUrl.'region/'.$lookUp;
+        $url = self::URL . 'region/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function ability($lookUp)
     {
-        $url = $this->baseUrl.'ability/'.$lookUp;
+        $url = self::URL . 'ability/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function characteristic($id)
     {
-        $url = $this->baseUrl.'characteristic/'.$id;
+        $url = self::URL . 'characteristic/' . $id;
 
         return $this->sendRequest($url);
     }
 
     public function eggGroup($lookUp)
     {
-        $url = $this->baseUrl.'egg-group/'.$lookUp;
+        $url = self::URL . 'egg-group/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function gender($lookUp)
     {
-        $url = $this->baseUrl.'gender/'.$lookUp;
+        $url = self::URL . 'gender/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function growthRate($lookUp)
     {
-        $url = $this->baseUrl.'growth-rate/'.$lookUp;
+        $url = self::URL . 'growth-rate/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function nature($lookUp)
     {
-        $url = $this->baseUrl.'nature/'.$lookUp;
+        $url = self::URL . 'nature/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokeathlonStat($lookUp)
     {
-        $url = $this->baseUrl.'pokeathlon-stat/'.$lookUp;
+        $url = self::URL . 'pokeathlon-stat/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokemon($lookUp)
     {
-        $url = $this->baseUrl.'pokemon/'.$lookUp;
+        $url = self::URL . 'pokemon/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokemonColor($lookUp)
     {
-        $url = $this->baseUrl.'pokemon-color/'.$lookUp;
+        $url = self::URL . 'pokemon-color/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokemonForm($lookUp)
     {
-        $url = $this->baseUrl.'pokemon-form/'.$lookUp;
+        $url = self::URL . 'pokemon-form/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokemonHabitat($lookUp)
     {
-        $url = $this->baseUrl.'pokemon-habitat/'.$lookUp;
+        $url = self::URL . 'pokemon-habitat/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokemonShape($lookUp)
     {
-        $url = $this->baseUrl.'pokemon-shape/'.$lookUp;
+        $url = self::URL . 'pokemon-shape/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokemonSpecies($lookUp)
     {
-        $url = $this->baseUrl.'pokemon-species/'.$lookUp;
+        $url = self::URL . 'pokemon-species/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokemonStat($lookUp)
     {
-        $url = $this->baseUrl.'stat/'.$lookUp;
+        $url = self::URL . 'stat/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function pokemonType($lookUp)
     {
-        $url = $this->baseUrl.'type/'.$lookUp;
+        $url = self::URL . 'type/' . $lookUp;
 
         return $this->sendRequest($url);
     }
 
     public function language($lookUp)
     {
-        $url = $this->baseUrl.'language/'.$lookUp;
+        $url = self::URL . 'language/' . $lookUp;
 
         return $this->sendRequest($url);
     }
