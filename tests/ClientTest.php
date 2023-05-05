833e8f93dac9b0dd9e08d5ea6f1049771390e0f9
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2023-05-05 09:57:55 -0300

    refactor: extract poke api to a property

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -5,20 +5,25 @@ use PokePHP\PokeApi;
 
 final class ClientTest extends \PHPUnit\Framework\TestCase
 {
+    private PokeApi $client;
+    
+    public function setUp(): void
+    {
+        $query = new Query('../responses/berry.json');
+        
+        $this->client = new PokeApi($query);
+    }
+    
     public function testResourceList()
     {
-        $poke = new PokeApi;
-
-        $response = $poke->search(Filter::EVOLUTION_CHAIN)->limit(20)->request();
+        $response = $this->client->search(Filter::EVOLUTION_CHAIN)->limit(20)->request();
 
         $this->assertJson($response, 'message');
     }
 
     public function testBerry()
     {
-        $poke = new PokeApi;
-
-        $response = $poke->search(Filter::BERRY, 1)->request();
+        $response = $this->client->search(Filter::BERRY, 1)->request();
 
         $this->assertJson($response, 'message');
     }
